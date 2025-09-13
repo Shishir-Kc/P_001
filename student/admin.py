b651f1d902fd58a71c0c64c522b7f3eb1b1c0758
Shishir_Kc <shishir.khatri.kc@gmail.com>
2025-09-13 20:24:49 +0545

    added filter , search and more in admin page

--- student/admin.py
+++ student/admin.py
@@ -2,7 +2,52 @@ from django.contrib import admin
 from . import models
 
 
-admin.site.register(models.Student_info)
+class Student_Admin(admin.ModelAdmin):
+    list_display= [
+        'first_name',
+        'last_name',
+        'age',
+        'student_code',
+        'student_class',
+        'Roll_num',
+        'emergency_contact',
 
 
-admin.site.register(models.Attendence)
+    ]
+    search_fields = [
+        'age',
+        'first_name',
+        'last_name',
+        'student_code',
+        'Roll_num',
+        'emergency_contact',
+    ]
+
+    list_filter=[
+        'age',
+        'student_class',
+        'Gender',
+        'joined',
+    ]
+
+   
+
+admin.site.register(models.Student_info,Student_Admin)
+
+
+
+class Attendence_Admin(admin.ModelAdmin):
+    list_display = [ 
+        'student',
+        'attendence',
+        'date_month',
+        'attended_class',
+    ]
+    list_filter=[
+        'attended_class',
+        'date_month',
+        'attendence',
+        'student__student_class'
+    ]
+
+admin.site.register(models.Attendence,Attendence_Admin)
